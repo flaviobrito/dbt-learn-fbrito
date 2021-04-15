e2c6fde274b076fecfe3325e38509c4d598433ec
Flávio Brito <flavio.brito@helloprint.com>
2021-04-15 09:35:34 +0000

    packages and jinja

---
+++ models/pivoted_payments.sql
@@ -0,0 +1,13 @@
+select
+order_id,
+{% for payment_method in ["bank_transfer", "credit_card", "gift_card"] %}
+
+sum(case when payment_method = '{{payment_method}}' then amount  else 0 end) as {{payment_method}}_amount
+    {%- if not loop.last -%}
+        ,
+    {% endif %}
+{% endfor %}
+,
+sum(amount) as total_amount
+from {{ ref('stg_payments') }}
+group by 1;
